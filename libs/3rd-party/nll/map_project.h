649683cec53399897dc9e899d56a620be4fd019e
fmassin <fmassin@sed.ethz.ch>
2017-01-27 09:17:03 +0000

    Make a separate package
    The separate package includes now only what's needed for scfinder.
    Hoping nothing is missing, more commits might follow.
    
    eewamps will be moved to main seiscomp in the near futur.
    
    The repo becomes now sed-addons and it is the right place for
    seiscomp-based SED products with restricted distribution (in
    dev or private).

--- libs/3rd-party/nll/map_project.h
+++
@@ -1,5 +0,0 @@
-
-int map_setup_proxy(int n_proj, char* ellipsoid_name);
-int vlamb(int n_proj, double rlong0, double rlat0, double pha, double phb);
-int lamb(int n_proj, double lon, double lat, double *x, double *y);
-int ilamb(int n_proj, double *lon, double *lat, double x, double y);
